b2956809193c9c6f08b253bfc9400f6c069cfd39
Justin <Justin@Xiongs-MBPR.local>
2016-03-13 16:55:30 +0800

    Add action to two FABs
    Photo taking action and photo picking action

--- app/src/main/java/io/macromind/fyp_hkust/MainActivity.java
+++ app/src/main/java/io/macromind/fyp_hkust/MainActivity.java
@@ -1,12 +1,27 @@
 package io.macromind.fyp_hkust;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 100;
+    static final int REQUEST_IMAGE_PICK = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton selectFab = (FloatingActionButton) findViewById(R.id.select_fab);
+        selectFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPicIntent = new Intent(Intent.ACTION_PICK);
+                pickPicIntent.setType("image/*");
+                startActivityForResult(pickPicIntent, REQUEST_IMAGE_PICK);
+            }
+        });
+
+        FloatingActionButton takePhotoFab = (FloatingActionButton) findViewById(R.id.take_photo_fab);
+        takePhotoFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePicIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePicIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePicIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,5 +82,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Image saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+            }
+        } else if (requestCode == REQUEST_IMAGE_PICK) {
+            if(resultCode == RESULT_OK){
+                Uri selectedImage = data.getData();
+                try {
+                    InputStream imageStream = getContentResolver().openInputStream(selectedImage);
+                    Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
+                }
+                catch (FileNotFoundException e) {
+                    throw new RuntimeException ("Failed to select", e);
+                }
+            }
+            else {
+
+            }
+        }
     }
 }
