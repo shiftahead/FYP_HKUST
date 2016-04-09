91fd752112ebf9fb739bb4647eba60e8a1bbe432
Justin <Justin@Xiongs-MBPR.local>
2016-04-09 17:50:08 +0800

    Delete redundant lib

--- jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := app
-LOCAL_SRC_FILES := \
-	/Users/Justin/Documents/FYP_HKUST/app/src/main/jniLibs/armeabi-v7a/libcaffe.so
-
-include $(PREBUILT_SHARED_LIBRARY)
